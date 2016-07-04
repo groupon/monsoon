24d51b76ea822853a856249dc7d6feffb8ebddef
Ariane van der Steldt <avandersteldt@groupon.com>
2016-07-04 18:28:32 +0100

    Document name rules for wavefront.

--- processors/wavefront/src/main/java/com/groupon/lex/metrics/processors/wavefront/WavefrontStrings.java
+++ processors/wavefront/src/main/java/com/groupon/lex/metrics/processors/wavefront/WavefrontStrings.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2016, Groupon, Inc.
- * All rights reserved. 
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
- * are met: 
+ * are met:
  *
  * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer. 
+ * this list of conditions and the following disclaimer.
  *
  * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution. 
+ * documentation and/or other materials provided with the distribution.
  *
  * Neither the name of GROUPON nor the names of its contributors may be
  * used to endorse or promote products derived from this software without
- * specific prior written permission. 
+ * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -53,14 +53,31 @@ public class WavefrontStrings {
 
     private WavefrontStrings() {}
 
-    /** Convert a name to a name that wavefront will accept. */
+    /**
+     * Convert a name to a name that wavefront will accept.
+     *
+     * Wavefront documentation specifies a metric consists of the characters [-a-z_.]
+     * Any disallowed characters are replaced with underscores.
+     */
     public static final String name(String name) {
         return name
                 .toLowerCase(Locale.ROOT)
                 .replaceAll("[^-a-z_.]", "_");
     }
 
-    /** Convert a group+metric to a wavefront name. */
+    /**
+     * Convert a group+metric to a wavefront name.
+     *
+     * Concatenates the paths of a Group and a Metric, separating each path element with a dot ('.').
+     *
+     * Example:
+     * - group path: [ 'example', 'group', 'path' ]
+     * - and metric: [ 'metric', 'name' ]
+     * are concatenated into "example.group.path.metric.name".
+     *
+     * The concatenated name is cleaned to only contain characters allowed by wavefront.
+     * (See the WavefrontString.name(String) function.)
+     */
     public static final String name(SimpleGroupPath group, MetricName metric) {
         return name(Stream.concat(group.getPath().stream(), metric.getPath().stream())
                 .collect(Collectors.joining(".")));
