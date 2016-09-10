df4b2033182ab5d5282aca0b0e9ccaaac2bdff76
Ariane van der Steldt <avandersteldt@groupon.com>
2016-09-10 18:09:49 +0100

    Create interfaces for generalized collector builders.
    - These builders declare the layout of the config statement, by implementing
      certain interfaces.
    - The setters are used to configure the builder and invoked by the parser.
    - The getters are used to recreate the config string for a collector.
    - The build method creates the appropriate collector.

---
+++ collectors/jmx/src/main/java/com/groupon/lex/metrics/jmx/JmxBuilder.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016, Groupon, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of GROUPON nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.groupon.lex.metrics.jmx;
+
+import com.groupon.lex.metrics.GroupGenerator;
+import com.groupon.lex.metrics.ResolverGroupGenerator;
+import com.groupon.lex.metrics.Tags;
+import com.groupon.lex.metrics.builders.collector.AcceptTagSet;
+import com.groupon.lex.metrics.builders.collector.CollectorBuilder;
+import com.groupon.lex.metrics.builders.collector.MainStringList;
+import com.groupon.lex.metrics.httpd.EndpointRegistration;
+import com.groupon.lex.metrics.lib.Any2;
+import com.groupon.lex.metrics.lib.Any3;
+import com.groupon.lex.metrics.resolver.NameBoundResolver;
+import static java.util.Collections.unmodifiableSortedSet;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import javax.management.MalformedObjectNameException;
+import javax.management.ObjectName;
+import lombok.Data;
+
+@Data
+public class JmxBuilder implements CollectorBuilder, MainStringList, AcceptTagSet {
+    private List<String> main;
+    private NameBoundResolver tagSet;
+
+    @Override
+    public GroupGenerator build(EndpointRegistration er) throws Exception {
+        final SortedSet<ObjectName> includes = getIncludes();
+
+        return new ResolverGroupGenerator(
+                tagSet,
+                (Map<Any2<Integer, String>, Any3<Boolean, Integer, String>> arg) -> {
+                    final String host = arg.getOrDefault(Any2.<Integer, String>right("host"), Any3.create3("localhost")).mapCombine(String::valueOf, String::valueOf, String::valueOf);
+                    final String port = arg.getOrDefault(Any2.<Integer, String>right("port"), Any3.create2(9999)).mapCombine(String::valueOf, String::valueOf, String::valueOf);
+
+                    final List<String> sublist = NameBoundResolver.indexToStringMap(arg).entrySet().stream()
+                            .sorted(Comparator.comparing(Map.Entry::getKey))
+                            .map(Map.Entry::getValue)
+                            .collect(Collectors.toList());
+                    final Tags tags = Tags.valueOf(NameBoundResolver.tagMap(arg));
+
+                    MetricListenerInstance listener = new MetricListenerInstance(new JmxClient("service:jmx:rmi:///jndi/rmi://" + host + ":" + port + "/jmxrmi"), includes, sublist, tags);
+                    listener.enable();
+                    return listener;
+                });
+    }
+
+    private SortedSet<ObjectName> getIncludes() throws MalformedObjectNameException {
+        final SortedSet<ObjectName> includes = new TreeSet<>();
+        for (String name : main)
+            includes.add(new ObjectName(name));
+        return unmodifiableSortedSet(includes);
+    }
+}
---
+++ intf/src/main/java/com/groupon/lex/metrics/builders/collector/CollectorBuilder.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2016, Groupon, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of GROUPON nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.groupon.lex.metrics.builders.collector;
+
+import com.groupon.lex.metrics.GroupGenerator;
+import com.groupon.lex.metrics.httpd.EndpointRegistration;
+
+/**
+ * Builder interface for a collector.
+ *
+ * An implementation of this class must:
+ * <ol>
+ *   <li>implement at least one of the Main* classes: MainNone, MainString or MainStringList</li>
+ * </ol>
+ *
+ * An implementation of this class may:
+ * <ol>
+ *   <li>implement one of AcceptAsPath, AcceptOptAsPath</li>
+ *   <li>implement AcceptTagSet</li>
+ * </ol>
+ *
+ * These extra interfaces are used to instruct the parser to handle the collector
+ * statements appropriately.
+ * @author ariane
+ */
+public interface CollectorBuilder {
+    /**
+     * Create the actual collector.
+     *
+     * The parser will call each of the setters from the Main* and Accept*
+     * base classes of the implementation exactly once, before calling this function.
+     * The function must be able to return multiple instances of the collector.
+     * @param er Endpoint registration API. Used for registering HTTP endpoints.
+     * @return A GroupGenerator implementation that performs the desired collection.
+     * @throws Exception If the implementation fails to create the collector.
+     */
+    public GroupGenerator build(EndpointRegistration er) throws Exception;
+}
