7fbcbff6767cff40b3d85a7e2ee3d2dac443e3c3
Ariane van der Steldt <avandersteldt@groupon.com>
2016-09-10 12:14:58 +0100

    Change RangeResolver syntax to int..int (inclusive).
    - RangeResolver(0, 0) -- 0..0, range containing single 0.
    - RangeResolver(0, 1) -- 0..1, range containing 0, 1.
    
    While here, implement tests for RangeResolver.

--- impl/src/main/java/com/groupon/lex/metrics/resolver/RangeResolver.java
+++ impl/src/main/java/com/groupon/lex/metrics/resolver/RangeResolver.java
@@ -3,48 +3,50 @@ package com.groupon.lex.metrics.resolver;
 import static com.groupon.lex.metrics.resolver.ResolverTuple.newTupleElement;
 import static java.lang.Integer.max;
 import java.util.ArrayList;
-import java.util.Collection;
+import static java.util.Collections.unmodifiableList;
 import java.util.List;
-import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
 /**
  * A resolver that emits a range of numbers.
+ *
+ * Bounds are inclusive.
  */
 @Getter
-@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class RangeResolver implements Resolver {
     private final int begin, end;
+    private final List<ResolverTuple> tuples;
 
-    public RangeResolver(int end) {
-        this(0, end);
+    public RangeResolver(int begin, int end) {
+        this.begin = begin;
+        this.end = end;
+        this.tuples = createTuples(begin, end);
     }
 
     @Override
-    public int getTupleWidth() {
-        return 1;
-    }
-
+    public int getTupleWidth() { return 1; }
     @Override
-    public Collection<ResolverTuple> getTuples() {
-        List<ResolverTuple> tuples = new ArrayList<>(max(end - begin, 0));
+    public String configString() { return begin + ".." + end; }
+
+    private static List<ResolverTuple> createTuples(int begin, int end) {
+        final ArrayList<ResolverTuple> tuples = new ArrayList<>(max(end - begin + 1, 0));
+
+        /*
+         * We need a for-loop: for (int i = begin; i <= end; ++i) { ... }
+         * But this won't work if end == Integer.MAX_VALUE, since i can never
+         * exceed Integer.MAX_VALUE (wrapping around to Integer.MIN_VALUE
+         * instead).
+         * So the equals-case is handled separately in an if-statement below.
+         */
         for (int i = begin; i < end; ++i)
             tuples.add(new ResolverTuple(newTupleElement(i)));
-        return tuples;
-    }
+        if (end >= begin)
+            tuples.add(new ResolverTuple(newTupleElement(end)));
 
-    @Override
-    public String configString() {
-        StringBuilder buf = new StringBuilder()
-                .append("range(");
-        if (begin != 0) {
-            buf
-                    .append(begin)
-                    .append(", ");
-        }
-        return buf
-                .append(end)
-                .append(')')
-                .toString();
+        return unmodifiableList(tuples);
     }
 }
