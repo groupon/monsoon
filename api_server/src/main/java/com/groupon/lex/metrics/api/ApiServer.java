a2e4f22a4f97f7ff71b478876e2dc5752b74e0f5
Ariane van der Steldt <avandersteldt@groupon.com>
2016-09-17 15:55:51 +0100

    Create API server that only listens.

--- api_server/src/main/java/com/groupon/lex/metrics/api/ApiServer.java
+++ api_server/src/main/java/com/groupon/lex/metrics/api/ApiServer.java
@@ -2,10 +2,19 @@ package com.groupon.lex.metrics.api;
 
 import com.groupon.lex.metrics.httpd.EndpointRegistration;
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import static java.util.Collections.singletonList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,24 +41,9 @@ public class ApiServer implements AutoCloseable, EndpointRegistration {
     private final ContextHandlerCollection context_ = new ContextHandlerCollection();
     private final ServletContextHandler servlet_handler;
 
-    public ApiServer(InetSocketAddress address) {
+    public ApiServer(Collection<? extends InetSocketAddress> addresses) {
         server_ = new Server();
-        /* Handle address logic. */ {
-            final ServerConnector server_connector_ = new ServerConnector(server_);
-            server_connector_.setReuseAddress(true);
-            if (address.getAddress() != null) {
-                if (!address.getAddress().isAnyLocalAddress()) {
-                    LOG.log(Level.INFO, "Binding API server address: {0}", address.getAddress().getHostAddress());
-                    server_connector_.setHost(address.getAddress().getHostAddress());
-                }
-            } else if (address.getHostString() != null) {
-                LOG.log(Level.INFO, "Binding API server address name: {0}", address.getHostString());
-                server_connector_.setHost(address.getHostString());
-            }
-            LOG.log(Level.INFO, "Binding API server port: {0}", address.getPort());
-            server_connector_.setPort(address.getPort());
-            server_.setConnectors(new Connector[]{ server_connector_ });
-        }
+        installListeners(server_, addresses);
 
         final HandlerList chain = new HandlerList();
         {
@@ -94,6 +88,10 @@ public class ApiServer implements AutoCloseable, EndpointRegistration {
         server_.addBean(new MonsoonErrorHandler());
     }
 
+    public ApiServer(InetSocketAddress address) {
+        this(singletonList(address));
+    }
+
     public void start() throws Exception {
         LOG.log(Level.INFO, "starting API server");
         server_.start();
@@ -113,5 +111,67 @@ public class ApiServer implements AutoCloseable, EndpointRegistration {
     public void addEndpoint(String pattern, HttpServlet servlet) {
         LOG.log(Level.INFO, "registering API endpoint {0} => {1}", new Object[]{pattern, servlet});
         servlet_handler.addServlet(new ServletHolder(servlet), pattern);
+    }
+
+    private static void installListeners(Server server, Collection<? extends InetSocketAddress> addresses) {
+        final List<Connector> connectors = new ArrayList<>(addresses.size());
+
+        for (InetSocketAddress address : addresses.stream()
+                .sorted(Comparator.comparing(InetSocketAddress::getAddress, new IPv4BeforeIPv6AddressComparator()))
+                .collect(Collectors.toList())) {
+            final ServerConnector server_connector = new ServerConnector(server);
+            server_connector.setReuseAddress(true);
+            if (address.getAddress() != null) {
+                if (!address.getAddress().isAnyLocalAddress()) {
+                    LOG.log(Level.INFO, "Binding API server address: {0}", address.getAddress().getHostAddress());
+                    server_connector.setHost(address.getAddress().getHostAddress());
+                }
+            } else if (address.getHostString() != null) {
+                LOG.log(Level.INFO, "Binding API server address name: {0}", address.getHostString());
+                server_connector.setHost(address.getHostString());
+            }
+            LOG.log(Level.INFO, "Binding API server port: {0}", address.getPort());
+            server_connector.setPort(address.getPort());
+            connectors.add(server_connector);
+        }
+
+        server.setConnectors(connectors.toArray(new Connector[connectors.size()]));
+    }
+
+    /**
+     * Comparator that compares addresses according to ordering:
+     * <ol>
+     *   <li>all IPv4 addresses</li>
+     *   <li>all IPv6 addresses</li>
+     *   <li>all remaining addresses</li>
+     * </ol>
+     */
+    private static class IPv4BeforeIPv6AddressComparator implements Comparator<InetAddress> {
+        @Override
+        public int compare(InetAddress o1, InetAddress o2) {
+            boolean o1_isIPv4 = o1 instanceof Inet4Address;
+            boolean o2_isIPv4 = o2 instanceof Inet4Address;
+            if (o1_isIPv4) {
+                if (o2_isIPv4)
+                    return 0;
+                else
+                    return -1;
+            } else if (o2_isIPv4) {
+                return 1;
+            }
+
+            boolean o1_isIPv6 = o1 instanceof Inet6Address;
+            boolean o2_isIPv6 = o2 instanceof Inet6Address;
+            if (o1_isIPv6) {
+                if (o2_isIPv6)
+                    return 0;
+                else
+                    return -1;
+            } else if (o2_isIPv6) {
+                return 1;
+            }
+
+            return 0;
+        }
     }
 }
---
+++ lib/src/main/java/com/groupon/lex/metrics/lib/ManyInetAddressOptionHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2016, Ariane van der Steldt
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of GROUPON nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.groupon.lex.metrics.lib;
+
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.OptionDef;
+import org.kohsuke.args4j.spi.OptionHandler;
+import org.kohsuke.args4j.spi.Parameters;
+import org.kohsuke.args4j.spi.Setter;
+
+/**
+ * Resolves a name to many inet socket addresses.
+ */
+public class ManyInetAddressOptionHandler extends OptionHandler<InetSocketAddress> {
+    public ManyInetAddressOptionHandler(CmdLineParser parser, OptionDef option, Setter<? super InetSocketAddress> setter) {
+        super(parser, option, setter);
+    }
+
+    protected int defaultPort() {
+        return 0;
+    }
+
+    @Override
+    public int parseArguments(Parameters prmtrs) throws CmdLineException {
+        final HostnamePort hp = HostnamePort.valueOf(prmtrs.getParameter(0), defaultPort());
+
+        try {
+            for (InetSocketAddress addr : hp.getAddressList())
+                setter.addValue(addr);
+            return 1;
+        } catch (UnknownHostException ex) {
+            throw new CmdLineException(owner, "unable to resolve " + hp.getHostname(), ex);
+        }
+    }
+
+    @Override
+    public String getDefaultMetaVariable() {
+        return "inet-address";
+    }
+
+    @Override
+    protected String print(InetSocketAddress v) {
+        return v.toString();
+    }
+}
