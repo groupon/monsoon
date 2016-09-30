b91a30a5b9875ecc7633ef26844edd51c0dc5613
Ariane van der Steldt <avandersteldt@groupon.com>
2016-09-30 13:58:53 +0100

    Document what the spec for time-spec is to be.

--- api_server/src/main/frontend/app/chart/chart-time-spec.ts
+++ api_server/src/main/frontend/app/chart/chart-time-spec.ts
@@ -1,3 +1,14 @@
+/*
+ * Time specifications:
+ * - begin time only (stream from begin until end-of-data).
+ * - begin time and end time (stream from begin until end).
+ * - begin time and duration (treat as end = begin + duration).
+ * - end time and duration (treat as begin = end - duration).
+ * - duration only (treat as begin = now() - duration, no end, no duration).
+ *
+ * Duration-only is to be interpreted as a sliding window, always displaying
+ * data between the end-of-data - duration .. end-of-data.
+ */
 export class ChartTimeSpec {
   constructor(public duration_seconds: Number, public end?: Date, public stepsize_seconds?: Number) {}
 
