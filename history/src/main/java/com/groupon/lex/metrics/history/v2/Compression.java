be6427352a1f0d56d4d7486ecea7e71e521d6d07
Ariane van der Steldt <ariane@stack.nl>
2016-10-14 23:50:26 +0100

    Switch to LZO for list files.

--- history/src/main/java/com/groupon/lex/metrics/history/v2/Compression.java
+++ history/src/main/java/com/groupon/lex/metrics/history/v2/Compression.java
@@ -18,7 +18,7 @@ public enum Compression {
     SNAPPY(header_flags.SNAPPY, in -> new SnappyReader(in), (out, hc) -> new SnappyWriter(out)),
     LZO(header_flags.LZO_1X1, in -> new LzoReader(in), (out, hc) -> new LzoWriter(out, hc));
 
-    public static final Compression DEFAULT_APPEND = GZIP;
+    public static final Compression DEFAULT_APPEND = LZO;
     public static final Compression DEFAULT_OPTIMIZED = LZO;
 
     public final int compressionFlag;
