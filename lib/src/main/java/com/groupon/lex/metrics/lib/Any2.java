1230815c4c26e845176e84ab07b5b699afe0f769
Ariane van der Steldt <avandersteldt@groupon.com>
2017-06-16 13:45:04 +0100

    Refactor Any2 to use 2 implementations.

--- lib/src/main/java/com/groupon/lex/metrics/lib/Any2.java
+++ lib/src/main/java/com/groupon/lex/metrics/lib/Any2.java
@@ -1,21 +1,21 @@
 /*
  * Copyright (c) 2016, Groupon, Inc.
- * All rights reserved. 
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
- * are met: 
+ * are met:
  *
  * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer. 
+ * this list of conditions and the following disclaimer.
  *
  * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution. 
+ * documentation and/or other materials provided with the distribution.
  *
  * Neither the name of GROUPON nor the names of its contributors may be
  * used to endorse or promote products derived from this software without
- * specific prior written permission. 
+ * specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
@@ -33,45 +33,84 @@ package com.groupon.lex.metrics.lib;
 
 import java.util.Optional;
 import java.util.function.Function;
-
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 /**
  *
  * @author ariane
  */
-
-@EqualsAndHashCode
-@ToString
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-public class Any2<T, U> {
-    private final T left;
-    private final U right;
+public abstract class Any2<T, U> {
+    private Any2() {}
 
     public static <T, U> Any2<T, U> left(@NonNull T t) {
-        return new Any2<>(t, null);
+        return new LeftImpl<>(t);
     }
 
     public static <T, U> Any2<T, U> right(@NonNull U u) {
-        return new Any2<>(null, u);
+        return new RightImpl(u);
     }
 
-    public Optional<T> getLeft() {
-        return Optional.ofNullable(left);
-    }
-    public Optional<U> getRight() {
-        return Optional.ofNullable(right);
-    }
-    
-    public <X, Y> Any2<X, Y> map(Function<? super T, X> left_fn, Function<? super U, Y> right_fn) {
-        return (left != null) ? left(left_fn.apply(left)) : right(right_fn.apply(right));
-    }
+    public abstract Optional<T> getLeft();
+    public abstract Optional<U> getRight();
+    public abstract <X, Y> Any2<X, Y> map(Function<? super T, X> left_fn, Function<? super U, Y> right_fn);
+    public abstract <X> X mapCombine(Function<? super T, ? extends X> left_fn, Function<? super U, ? extends X> right_fn);
 
-    public <X> X mapCombine(Function<? super T, ? extends X> left_fn, Function<? super U, ? extends X> right_fn) {
-        return (left != null) ? left_fn.apply(left) : right_fn.apply(right);
-    }
+    @RequiredArgsConstructor
+    @ToString
+    @EqualsAndHashCode(callSuper = false)
+    private static final class LeftImpl<T, U> extends Any2<T, U> {
+        @NonNull
+        private final T v;
+
+        @Override
+        public Optional<T> getLeft() {
+            return Optional.of(v);
+        }
+
+        @Override
+        public Optional<U> getRight() {
+            return Optional.empty();
+        }
+
+        @Override
+        public <X, Y> Any2<X, Y> map(Function<? super T, X> left_fn, Function<? super U, Y> right_fn) {
+            return Any2.left(left_fn.apply(v));
+        }
+
+        @Override
+        public <X> X mapCombine(Function<? super T, ? extends X> left_fn, Function<? super U, ? extends X> right_fn) {
+            return left_fn.apply(v);
+        }
+    };
+
+    @RequiredArgsConstructor
+    @ToString
+    @EqualsAndHashCode(callSuper = false)
+    private static final class RightImpl<T, U> extends Any2<T, U> {
+        @NonNull
+        private final U v;
+
+        @Override
+        public Optional<T> getLeft() {
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<U> getRight() {
+            return Optional.of(v);
+        }
+
+        @Override
+        public <X, Y> Any2<X, Y> map(Function<? super T, X> left_fn, Function<? super U, Y> right_fn) {
+            return Any2.right(right_fn.apply(v));
+        }
+
+        @Override
+        public <X> X mapCombine(Function<? super T, ? extends X> left_fn, Function<? super U, ? extends X> right_fn) {
+            return right_fn.apply(v);
+        }
+    };
 }
