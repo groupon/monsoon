ae5b466eb0971986096b5f27ccd646b92c1095ec
Ariane van der Steldt <ariane@stack.nl>
2017-06-16 22:31:27 +0100

    Hook up parsing to TimeSeriesMetricExpression::valueOf() methods.

--- expr/src/main/java/com/groupon/lex/metrics/timeseries/TimeSeriesMetricExpression.java
+++ expr/src/main/java/com/groupon/lex/metrics/timeseries/TimeSeriesMetricExpression.java
@@ -34,9 +34,25 @@ package com.groupon.lex.metrics.timeseries;
 import com.groupon.lex.metrics.MetricValue;
 import com.groupon.lex.metrics.timeseries.expression.Context;
 import static com.groupon.lex.metrics.timeseries.expression.Priorities.BRACKETS;
+import com.groupon.lex.metrics.timeseries.parser.Expression;
+import com.groupon.lex.metrics.timeseries.parser.ExpressionLexer;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import static java.util.Collections.EMPTY_LIST;
+import static java.util.Collections.unmodifiableList;
+import java.util.List;
 import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import lombok.Getter;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.BufferedTokenStream;
+import org.antlr.v4.runtime.Recognizer;
 
 /**
  *
@@ -62,7 +78,9 @@ public interface TimeSeriesMetricExpression extends Function<Context<?>, TimeSer
         }
 
         @Override
-        public Collection<TimeSeriesMetricExpression> getChildren() { return Collections.EMPTY_LIST; }
+        public Collection<TimeSeriesMetricExpression> getChildren() {
+            return Collections.EMPTY_LIST;
+        }
     };
 
     public static final TimeSeriesMetricExpression FALSE = new TimeSeriesMetricExpression() {
@@ -84,7 +102,9 @@ public interface TimeSeriesMetricExpression extends Function<Context<?>, TimeSer
         }
 
         @Override
-        public Collection<TimeSeriesMetricExpression> getChildren() { return Collections.EMPTY_LIST; }
+        public Collection<TimeSeriesMetricExpression> getChildren() {
+            return Collections.EMPTY_LIST;
+        }
     };
 
     @Override
@@ -94,9 +114,120 @@ public interface TimeSeriesMetricExpression extends Function<Context<?>, TimeSer
 
     /**
      * @return A filter that describes how far back the expression may reach in
-     *     the collection history.
+     * the collection history.
      */
     public default ExpressionLookBack getLookBack() {
         return ExpressionLookBack.EMPTY.andThen(getChildren().stream().map(TimeSeriesMetricExpression::getLookBack));
+    }
+
+    /**
+     * Read expression from string.
+     *
+     * @param str A string containing an expression.
+     * @return A TimeSeriesMetricExpression corresponding to the parsed input
+     * from the reader.
+     * @throws
+     * com.groupon.lex.metrics.timeseries.TimeSeriesMetricExpression.ParseException
+     * on invalid expression.
+     */
+    public static TimeSeriesMetricExpression valueOf(String str) throws ParseException {
+        try {
+            return valueOf(new StringReader(str));
+        } catch (IOException ex) {
+            throw new IllegalStateException("StringReader IO error?", ex);
+        }
+    }
+
+    /**
+     * Read expression from reader.
+     *
+     * @param reader A reader supplying the input of an expression.
+     * @return A TimeSeriesMetricExpression corresponding to the parsed input
+     * from the reader.
+     * @throws IOException on IO errors from the reader.
+     * @throws
+     * com.groupon.lex.metrics.timeseries.TimeSeriesMetricExpression.ParseException
+     * on invalid expression.
+     */
+    public static TimeSeriesMetricExpression valueOf(Reader reader) throws IOException, ParseException {
+        final Logger LOG = Logger.getLogger(TimeSeriesMetricExpression.class.getName());
+
+        class DescriptiveErrorListener extends BaseErrorListener {
+            public List<String> errors = new ArrayList<>();
+
+            @Override
+            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol,
+                                    int line, int charPositionInLine,
+                                    String msg, org.antlr.v4.runtime.RecognitionException e) {
+                LOG.log(Level.INFO, "Parse error: {0}:{1} -> {2}", new Object[]{line, charPositionInLine, msg});
+                errors.add(String.format("%d:%d: %s", line, charPositionInLine, msg));
+            }
+        }
+
+        final DescriptiveErrorListener error_listener = new DescriptiveErrorListener();
+
+        final ExpressionLexer lexer = new ExpressionLexer(new ANTLRInputStream(reader));
+        lexer.removeErrorListeners();
+        lexer.addErrorListener(error_listener);
+
+        final Expression parser = new Expression(new BufferedTokenStream(lexer));
+        parser.removeErrorListeners();
+        parser.addErrorListener(error_listener);
+
+        final Expression.ExprContext expr;
+        try {
+            expr = parser.expr();
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "parser yielded exceptional return", ex);
+            if (!error_listener.errors.isEmpty())
+                throw new ParseException(error_listener.errors, ex);
+            else
+                throw ex;
+        }
+
+        if (!error_listener.errors.isEmpty()) {
+            if (expr.exception != null)
+                throw new ParseException(error_listener.errors, expr.exception);
+            throw new ParseException(error_listener.errors);
+        } else if (expr.exception != null) {
+            throw new ParseException(expr.exception);
+        }
+        return expr.s;
+    }
+
+    /**
+     * Exception indicating an invalid expression. The parse errors are recorded
+     * as messages and can be retrieved using getParseErrors().
+     */
+    public static class ParseException extends Exception {
+        @Getter
+        private final List<String> parseErrors;
+
+        public ParseException(List<String> parseErrors) {
+            super();
+            this.parseErrors = unmodifiableList(parseErrors);
+        }
+
+        public ParseException(List<String> parseErrors, Throwable cause) {
+            super(cause);
+            this.parseErrors = unmodifiableList(parseErrors);
+        }
+
+        public ParseException(List<String> parseErrors, String message, Throwable cause) {
+            super(message, cause);
+            this.parseErrors = unmodifiableList(parseErrors);
+        }
+
+        public ParseException() {
+            this(EMPTY_LIST);
+        }
+
+        public ParseException(Throwable cause) {
+            this(EMPTY_LIST, cause);
+        }
+
+        public ParseException(String message, Throwable cause) {
+            this(EMPTY_LIST, message, cause);
+        }
     }
 }
