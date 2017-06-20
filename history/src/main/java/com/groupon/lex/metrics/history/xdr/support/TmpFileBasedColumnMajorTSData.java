8dd66980c9b77ecb93b68b74aaa5fd8dda4688cf
Ariane van der Steldt <ariane@stack.nl>
2017-06-20 18:59:14 +0100

    Get back the speed from earlier.

--- history/src/main/java/com/groupon/lex/metrics/history/xdr/support/TmpFileBasedColumnMajorTSData.java
+++ history/src/main/java/com/groupon/lex/metrics/history/xdr/support/TmpFileBasedColumnMajorTSData.java
@@ -1,5 +1,6 @@
 package com.groupon.lex.metrics.history.xdr.support;
 
+import com.google.common.collect.Iterators;
 import com.groupon.lex.metrics.GroupName;
 import com.groupon.lex.metrics.MetricName;
 import com.groupon.lex.metrics.MetricValue;
@@ -14,32 +15,28 @@ import com.groupon.lex.metrics.history.xdr.ColumnMajorTSData;
 import com.groupon.lex.metrics.lib.GCCloseable;
 import com.groupon.lex.metrics.lib.SimpleMapEntry;
 import com.groupon.lex.metrics.timeseries.TimeSeriesCollection;
-import gnu.trove.iterator.TIntObjectIterator;
 import gnu.trove.iterator.TLongIterator;
 import gnu.trove.list.TLongList;
 import gnu.trove.list.array.TLongArrayList;
-import gnu.trove.map.TIntObjectMap;
-import gnu.trove.map.hash.TIntObjectHashMap;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.AbstractSet;
 import java.util.ArrayList;
 import java.util.Collection;
+import static java.util.Collections.emptyIterator;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
 import static java.util.Collections.singleton;
 import static java.util.Collections.synchronizedSet;
 import static java.util.Collections.unmodifiableSet;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
-import java.util.Spliterators;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -88,21 +85,12 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
                             .forEach(tsv -> {
                                 final GroupWriter groupWriter = writers.computeIfAbsent(
                                         tsv.getGroup(),
-                                        (g) -> {
-                                            try {
-                                                return new GroupWriter();
-                                            } catch (IOException ex) {
-                                                throw new RuntimeIOException(ex);
-                                            }
-                                        });
+                                        (g) -> new GroupWriter());
 
                                 try {
-                                    fixBacklog(groupWriter);
+                                    groupWriter.fixBacklog(timestamps.size());
 
-                                    assert groupWriter.size() == timestamps.size();
                                     groupWriter.add(tsv.getMetrics());
-                                } catch (OncRpcException ex) {
-                                    throw new RuntimeIOException(new IOException("encoding problem", ex));
                                 } catch (IOException ex) {
                                     throw new RuntimeIOException(ex);
                                 }
@@ -124,36 +112,27 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
          * We want to keep all the Group instances to have the same number of
          * maps as the timestamps list, so we can zip the two together.
          */
-        private void fixBacklog() throws OncRpcException, IOException {
-            for (GroupWriter groupWriter : writers.values()) {
-                fixBacklog(groupWriter);
+        private void fixBacklog() throws IOException {
+            try {
+                writers.values().parallelStream()
+                        .forEach(groupWriter -> {
+                            try {
+                                groupWriter.fixBacklog(timestamps.size());
+                            } catch (IOException ex) {
+                                throw new RuntimeIOException(ex);
+                            }
+                        });
+            } catch (RuntimeIOException ex) {
+                throw ex.getEx();
             }
-        }
-
-        /**
-         * Fill in the backlog. Backlog can occur if a group is not always
-         * present, causing us to miss writes that should occur.
-         */
-        private void fixBacklog(GroupWriter groupWriter) throws OncRpcException, IOException {
-            for (int backlogIdx = groupWriter.size();
-                 backlogIdx < timestamps.size();
-                 ++backlogIdx)
-                groupWriter.add(emptyMap());
         }
 
         public TmpFileBasedColumnMajorTSData build() throws IOException {
-            try {
-                fixBacklog();
+            fixBacklog();
 
-                final Map<GroupName, Group> groups = writers.entrySet().stream()
-                        .peek(groupWriterEntry -> {
-                            assert groupWriterEntry.getValue().size() == timestamps.size();
-                        })
-                        .collect(Collectors.toMap(Map.Entry::getKey, groupWriterEntry -> groupWriterEntry.getValue().asReader()));
-                return new TmpFileBasedColumnMajorTSData(timestamps, groups, timestampsByGroup);
-            } catch (OncRpcException ex) {
-                throw new IOException("encoding problem", ex);
-            }
+            final Map<GroupName, Group> groups = writers.entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, groupWriterEntry -> groupWriterEntry.getValue().asReader()));
+            return new TmpFileBasedColumnMajorTSData(timestamps, groups, timestampsByGroup);
         }
     }
 
@@ -188,7 +167,7 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
     @Override
     public Map<DateTime, MetricValue> getMetricValues(GroupName group, MetricName metric) {
         final Group groupData = groups.get(group);
-        if (groupData == null)
+        if (groupData == null || !groupData.getMetricNames().contains(metric))
             return emptyMap();
 
         return new MetricValuesMap(timestamps, groupData, metric);
@@ -217,14 +196,11 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
 
             @Override
             public Iterator<Map.Entry<DateTime, MetricValue>> iterator() {
-                return StreamSupport.stream(Spliterators.spliteratorUnknownSize(groupData.iterator(timestamps), 0), false)
-                        .map(timestampedTsv -> {
-                            final MetricValue metricValue = timestampedTsv.getTsv().get(metric);
-                            if (metricValue == null) return null;
-                            return SimpleMapEntry.create(timestampedTsv.getTimestamp(), metricValue);
-                        })
-                        .filter(entry -> entry != null)
-                        .iterator();
+                return Iterators.transform(
+                        Iterators.filter(
+                                groupData.iterator(timestamps, metric),
+                                timestampedMetric -> timestampedMetric.getValue().isPresent()),
+                        timestampedMetric -> SimpleMapEntry.create(timestampedMetric.getTimestamp(), timestampedMetric.getValue().get()));
             }
 
             @Override
@@ -235,53 +211,127 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
     }
 
     private static class GroupWriter {
-        private final Set<MetricName> metricNames = new HashSet<>();
-        private final GCCloseable<TmpFile<XdrAbleMetricMap>> tmpFile;
-        private final DictionaryForWrite dictionary = new DictionaryForWrite();
+        private final Map<MetricName, MetricWriter> metrics = new ConcurrentHashMap<>();
 
-        public GroupWriter() throws IOException {
-            this.tmpFile = new GCCloseable<>(new TmpFile<>(TMP_FILE_COMPRESSION));
+        public void add(Map<MetricName, MetricValue> tsv) throws IOException {
+            try {
+                tsv.entrySet().parallelStream()
+                        .forEach(entry -> {
+                            final MetricWriter f = metrics.computeIfAbsent(
+                                    entry.getKey(),
+                                    (m) -> {
+                                        try {
+                                            return new MetricWriter();
+                                        } catch (IOException ex) {
+                                            throw new RuntimeIOException(ex);
+                                        }
+                                    });
+
+                            try {
+                                f.add(entry.getValue());
+                            } catch (IOException ex) {
+                                throw new RuntimeIOException(ex);
+                            }
+                        });
+            } catch (RuntimeIOException ex) {
+                throw ex.getEx();
+            }
         }
 
-        public void add(Map<MetricName, MetricValue> tsv) throws OncRpcException, IOException {
-            metricNames.addAll(tsv.keySet());
-            tmpFile.get().add(new XdrAbleMetricMap(dictionary, tsv));
-        }
-
-        public int size() {
-            return tmpFile.get().size();
+        public void fixBacklog(int padUntil) throws IOException {
+            try {
+                metrics.values().parallelStream()
+                        .forEach(f -> {
+                            try {
+                                f.fixBacklog(padUntil);
+                            } catch (IOException ex) {
+                                throw new RuntimeIOException(ex);
+                            }
+                        });
+            } catch (RuntimeIOException ex) {
+                throw ex.getEx();
+            }
         }
 
         public Group asReader() {
-            return new Group(metricNames, tmpFile);
+            return new Group(metrics.entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().asReader())));
         }
     }
 
     @AllArgsConstructor
     private static class Group {
-        @Getter
-        private final Set<MetricName> metricNames;
-        private final GCCloseable<TmpFile<XdrAbleMetricMap>> tmpFile;
+        private final Map<MetricName, Metric> metrics;
 
-        public Iterator<TimestampedTsv> iterator(TLongList timestamps) {
-            return new IteratorImpl(tmpFile, timestamps);
+        public Set<MetricName> getMetricNames() {
+            return metrics.keySet();
         }
 
-        private static class IteratorImpl implements Iterator<TimestampedTsv> {
-            private final TLongIterator timestampIter;
-            private final Iterator<XdrAbleMetricMap> inner;
+        public Iterator<TimestampedMetric> iterator(TLongList timestamps, MetricName metricName) {
+            final Metric m = metrics.get(metricName);
+            if (m == null) return emptyIterator();
+            return m.iterator(timestamps);
+        }
+    }
+
+    private static class MetricWriter {
+        private final GCCloseable<TmpFile<XdrAbleMetricEntry>> tmpFile;
+        private final DictionaryForWrite dictionary = new DictionaryForWrite();
+
+        public MetricWriter() throws IOException {
+            this.tmpFile = new GCCloseable<>(new TmpFile<>(TMP_FILE_COMPRESSION));
+        }
+
+        public void add(@NonNull MetricValue metric) throws IOException {
+            try {
+                tmpFile.get().add(new XdrAbleMetricEntry(dictionary, Optional.of(metric)));
+            } catch (OncRpcException ex) {
+                throw new IOException(ex);
+            }
+        }
+
+        public void fixBacklog(int padUntil) throws IOException {
+            try {
+                for (int i = tmpFile.get().size(); i < padUntil; ++i)
+                    tmpFile.get().add(new XdrAbleMetricEntry(dictionary, Optional.empty()));
+            } catch (OncRpcException ex) {
+                throw new IOException(ex);
+            }
+        }
+
+        public Metric asReader() {
+            return new Metric(tmpFile);
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class Metric {
+        private final GCCloseable<TmpFile<XdrAbleMetricEntry>> tmpFile;
+
+        public Iterator<TimestampedMetric> iterator(TLongList timestamps) {
+            final TLongIterator timestampIter = timestamps.iterator();
+            return Iterators.transform(
+                    iterator(),
+                    metric -> new TimestampedMetric(new DateTime(timestampIter.next(), DateTimeZone.UTC), metric));
+        }
+
+        public Iterator<Optional<MetricValue>> iterator() {
+            return new IteratorImpl(tmpFile);
+        }
+
+        private static class IteratorImpl implements Iterator<Optional<MetricValue>> {
+            private final Iterator<XdrAbleMetricEntry> inner;
             private DictionaryDelta dictionary = new DictionaryDelta();
 
             /*
              * Bind the lifetime of tmpFile to the lifetime of this iterator.
              */
-            private final GCCloseable<TmpFile<XdrAbleMetricMap>> tmpFile;
+            private final GCCloseable<TmpFile<XdrAbleMetricEntry>> tmpFile;
 
-            public IteratorImpl(@NonNull GCCloseable<TmpFile<XdrAbleMetricMap>> tmpFile, @NonNull TLongList timestamps) {
+            public IteratorImpl(@NonNull GCCloseable<TmpFile<XdrAbleMetricEntry>> tmpFile) {
                 this.tmpFile = tmpFile;
-                this.timestampIter = timestamps.iterator();
                 try {
-                    inner = this.tmpFile.get().iterator(XdrAbleMetricMap::new);
+                    inner = this.tmpFile.get().iterator(XdrAbleMetricEntry::new);
                 } catch (IOException | OncRpcException ex) {
                     throw new DecodingException("cannot read: decoding failed", ex);
                 }
@@ -293,86 +343,69 @@ public class TmpFileBasedColumnMajorTSData implements ColumnMajorTSData {
             }
 
             @Override
-            public TimestampedTsv next() {
-                final DateTime ts = new DateTime(timestampIter.next(), DateTimeZone.UTC);
-                final Map<MetricName, MetricValue> metricMap = inner.next()
+            public Optional<MetricValue> next() {
+                return inner.next()
                         .decode(dictionary, (updatedDictionary) -> dictionary = updatedDictionary);
-                return new TimestampedTsv(ts, metricMap);
             }
         }
     }
 
-    private static class XdrAbleMetricMap implements XdrAble {
-        private TIntObjectMap<metric_value> metrics;
+    private static class XdrAbleMetricEntry implements XdrAble {
+        private boolean present = false;
+        private metric_value metric;
         private dictionary_delta dd;
 
-        public XdrAbleMetricMap() {
+        public XdrAbleMetricEntry() {
             /* SKIP */
         }
 
-        public XdrAbleMetricMap(@NonNull DictionaryForWrite dictionary, @NonNull Map<MetricName, MetricValue> metrics) {
-            this.metrics = new TIntObjectHashMap<>();
-
-            metrics.entrySet().stream()
-                    .forEach(entry -> {
-                        final int metricName = dictionary.getPathTable().getOrCreate(entry.getKey().getPath());
-                        final metric_value metricValue = ToXdr.metricValue(entry.getValue(), dictionary.getStringTable()::getOrCreate);
-                        this.metrics.put(metricName, metricValue);
-                    });
-
-            this.dd = dictionary.encode();
+        public XdrAbleMetricEntry(@NonNull DictionaryForWrite dictionary, @NonNull Optional<MetricValue> value) {
+            this.present = value.isPresent();
+            if (value.isPresent()) {
+                this.metric = ToXdr.metricValue(value.get(), dictionary.getStringTable()::getOrCreate);
+                this.dd = dictionary.encode();
+            }
             dictionary.reset();
         }
 
-        public Map<MetricName, MetricValue> decode(DictionaryDelta inputDictionary, Consumer<DictionaryDelta> updateDictionary) {
-            final Map<MetricName, MetricValue> metricMap = new HashMap<>();
-            final DictionaryDelta dictionary = new DictionaryDelta(dd, inputDictionary);
+        public Optional<MetricValue> decode(DictionaryDelta inputDictionary, Consumer<DictionaryDelta> updateDictionary) {
+            if (!present) return Optional.empty();
 
-            metrics.forEachEntry((metricName, metricValue) -> {
-                metricMap.put(MetricName.valueOf(dictionary.getPath(metricName)),
-                        FromXdr.metricValue(metricValue, dictionary::getString));
-                return true;
-            });
+            final DictionaryDelta dictionary = new DictionaryDelta(dd, inputDictionary);
+            final MetricValue result = FromXdr.metricValue(metric, dictionary::getString);
 
             updateDictionary.accept(dictionary);
-            return metricMap;
+            return Optional.of(result);
         }
 
         @Override
         public void xdrEncode(XdrEncodingStream stream) throws OncRpcException, IOException {
-            stream.xdrEncodeInt(metrics.size());
-
-            final TIntObjectIterator<metric_value> iter = metrics.iterator();
-            while (iter.hasNext()) {
-                iter.advance();
-                stream.xdrEncodeInt(iter.key());
-                iter.value().xdrEncode(stream);
+            stream.xdrEncodeBoolean(present);
+            if (present) {
+                dd.xdrEncode(stream);
+                metric.xdrEncode(stream);
             }
-
-            dd.xdrEncode(stream);
         }
 
         @Override
         public void xdrDecode(XdrDecodingStream stream) throws OncRpcException, IOException {
-            final int sz = stream.xdrDecodeInt();
-
-            metrics = new TIntObjectHashMap<>();
-            for (int i = 0; i < sz; ++i) {
-                final int metricName = stream.xdrDecodeInt();
-                final metric_value metricValue = new metric_value(stream);
-                metrics.put(metricName, metricValue);
+            present = stream.xdrDecodeBoolean();
+            if (!present) {
+                metric = null;
+                dd = null;
+            } else {
+                dd = new dictionary_delta(stream);
+                metric = new metric_value(stream);
             }
-
-            dd = new dictionary_delta(stream);
         }
     }
 
     @Value
-    private static class TimestampedTsv {
+    private static class TimestampedMetric {
         @NonNull
         private final DateTime timestamp;
         @NonNull
-        private final Map<MetricName, MetricValue> tsv;
+        private final Optional<MetricValue> value;
     }
 
     @RequiredArgsConstructor
